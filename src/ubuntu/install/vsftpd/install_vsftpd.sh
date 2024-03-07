f84ab60ad9a4f973af895e13221a05400a6c223b
dooshub <162514331+dooshub@users.noreply.github.com>
2024-03-07 07:53:41 +0100

    Update install_vsftpd.sh

--- src/ubuntu/install/vsftpd/install_vsftpd.sh
+++ src/ubuntu/install/vsftpd/install_vsftpd.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 set -ex
 
-CHROME_ARGS="--password-store=basic --no-sandbox --ignore-gpu-blocklist --user-data-dir --no-first-run --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'"
-CHROME_VERSION=$1
+RDM_ARGS="--password-store=basic --no-sandbox --ignore-gpu-blocklist --user-data-dir --no-first-run --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'"
+RDM_VERSION=$1
 
 ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
 if [ "$ARCH" == "arm64" ] ; then
-  echo "Chrome not supported on arm64, skipping Chrome installation"
+  echo "rdm not supported on arm64, skipping rdm installation"
   exit 0
 fi	
 
 if [[ "${DISTRO}" == @(centos|oracle8|rockylinux9|rockylinux8|oracle9|almalinux9|almalinux8) ]]; then
-  if [ ! -z "${CHROME_VERSION}" ]; then
-    wget https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-${CHROME_VERSION}.x86_64.rpm -O chrome.rpm
+  if [ ! -z "${RDM_VERSION}" ]; then
+    wget https://cdn.devolutions.net/download/Linux/RDM/2024.1.0.6/RemoteDesktopManager_${RDM_VERSION}_x86_64.rpm -O rdm.rpm
   else
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O chrome.rpm
+    wget https://cdn.devolutions.net/download/Linux/RDM/2024.1.0.6/RemoteDesktopManager_2024.1.0.6_x86_64.rpm -O rdm.rpm
   fi
   if [[ "${DISTRO}" == @(oracle8|rockylinux9|rockylinux8|oracle9|almalinux9|almalinux8) ]]; then
-    dnf localinstall -y chrome.rpm
+    dnf localinstall -y rdm.rpm
     if [ -z ${SKIP_CLEAN+x} ]; then
       dnf clean all
     fi
   else
-    yum localinstall -y chrome.rpm
+    yum localinstall -y rdm.rpm
     if [ -z ${SKIP_CLEAN+x} ]; then
       yum clean all
     fi
   fi
-  rm chrome.rpm
+  rm rdm.rpm
 elif [ "${DISTRO}" == "opensuse" ]; then
   zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
   wget https://dl.google.com/linux/linux_signing_key.pub
@@ -39,13 +39,13 @@ elif [ "${DISTRO}" == "opensuse" ]; then
   fi
 else
   apt-get update
-  if [ ! -z "${CHROME_VERSION}" ]; then
-    wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb -O chrome.deb
+  if [ ! -z "${RDM_VERSION}" ]; then
+    wget https://cdn.devolutions.net/download/Linux/RDM/2024.1.0.6/RemoteDesktopManager_${RDM_VERSION}_amd64.deb -O rdm.deb
   else
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+    wget https://cdn.devolutions.net/download/Linux/RDM/2024.1.0.6/RemoteDesktopManager_2024.1.0.6_amd64.deb -O rdm.deb
   fi
-  apt-get install -y ./chrome.deb
-  rm chrome.deb
+  apt-get install -y ./rdm.deb
+  rm rdm.deb
   if [ -z ${SKIP_CLEAN+x} ]; then
     apt-get autoclean
     rm -rf \
@@ -54,26 +54,26 @@ else
   fi
 fi
 
-sed -i 's/-stable//g' /usr/share/applications/google-chrome.desktop
+sed -i 's/-stable//g' /usr/share/applications/remotedesktopmanager.desktop
 
-cp /usr/share/applications/google-chrome.desktop $HOME/Desktop/
-chown 1000:1000 $HOME/Desktop/google-chrome.desktop
-chmod +x $HOME/Desktop/google-chrome.desktop
+cp /usr/share/applications/remotedesktopmanager.desktop $HOME/Desktop/
+chown 1000:1000 $HOME/Desktop/remotedesktopmanager.desktop
+chmod +x $HOME/Desktop/remotedesktopmanager.desktop
 
-mv /usr/bin/google-chrome /usr/bin/google-chrome-orig
-cat >/usr/bin/google-chrome <<EOL
+mv /usr/bin/remotedesktopmanager /usr/bin/remotedesktopmanager-orig
+cat >/usr/bin/remotedesktopmanager <<EOL
 #!/usr/bin/env bash
-if ! pgrep chrome > /dev/null;then
-  rm -f \$HOME/.config/google-chrome/Singleton*
+if ! pgrep rdm > /dev/null;then
+  rm -f \$HOME/.rdm/Singleton*
 fi
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/google-chrome/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/google-chrome/Default/Preferences
 if [ -f /opt/VirtualGL/bin/vglrun ] && [ ! -z "\${KASM_EGL_CARD}" ] && [ ! -z "\${KASM_RENDERD}" ] && [ -O "\${KASM_RENDERD}" ] && [ -O "\${KASM_EGL_CARD}" ] ; then
     echo "Starting Chrome with GPU Acceleration on EGL device \${KASM_EGL_CARD}"
-    vglrun -d "\${KASM_EGL_CARD}" /opt/google/chrome/google-chrome ${CHROME_ARGS} "\$@" 
+    vglrun -d "\${KASM_EGL_CARD}" /opt/google/chrome/google-chrome ${RDM_ARGS} "\$@" 
 else
     echo "Starting Chrome"
-    /opt/google/chrome/google-chrome ${CHROME_ARGS} "\$@"
+    /opt/google/chrome/google-chrome ${RDM_ARGS} "\$@"
 fi
 EOL
 chmod +x /usr/bin/google-chrome
@@ -97,7 +97,7 @@ EOF
 else
   sed -i 's@exec -a "$0" "$HERE/google-chrome" "$\@"@@g' /usr/bin/x-www-browser
   cat >>/usr/bin/x-www-browser <<EOL
-  exec -a "\$0" "\$HERE/chrome" "${CHROME_ARGS}"  "\$@"
+  exec -a "\$0" "\$HERE/chrome" "${RDM_ARGS}"  "\$@"
 EOL
 fi
 
